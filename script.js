f0508a55dd7ee2942ad5fc5a950169c570dc3425
Sophie Martina Margarete Brunner <95913912+sophiebrunner@users.noreply.github.com>
2022-05-16 01:06:08 +0200

    feat: add logic connection between the pw-checks using if-else-statement

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
-const firstPassword = document.querySelector("#first-password");
-const secondPassword = document.querySelector("#second-password");
-secondPassword.addEventListener("keyup", passwordCheck);
-firstPassword.addEventListener("keyup", passwordCheck);
-
-//Toggle button to show and hide passwords
-const switchPWButton = document.querySelector("#switch-pw-visibility");
-switchPWButton.addEventListener("click", () => {
-  const firstPassword = document.querySelector("#first-password");
-  const secondPassword = document.querySelector("#second-password");
-
-  if (firstPassword.type && secondPassword.type === "password") {
-    firstPassword.type = "text";
-    secondPassword.type = "text";
-    switchPWButton.innerText = "Hide Passwords";
-  } else if (firstPassword.type && secondPassword.type === "text") {
-    firstPassword.type = "password";
-    secondPassword.type = "password";
-    switchPWButton.innerText = "Show Passwords";
-  }
-});
-
-function passwordCheck() {
+function renderPasswordCheck() {
   //Store both input-elements in objects
   const firstPassword = document.querySelector("#first-password");
   const secondPassword = document.querySelector("#second-password");
 
-  //First check: Passwords are euqal
   const equalityCheck = document.querySelector("#equality-check");
-  if (firstPassword.value === secondPassword.value) {
-    equalityCheck.innerHTML = "Passwords are equal ✅";
+  const lowerCaseCheck = document.querySelector("#lower-case-check");
+  const upperCaseCheck = document.querySelector("#upper-case-check");
+  const containsNumbersCheck = document.querySelector(
+    "#contains-numbers-check"
+  );
+  const minimumLengthCheck = document.querySelector("#minimum-length-check");
+
+  secondPassword.addEventListener("input", () => {
+    if (firstPassword.value === secondPassword.value) {
+      passwordCheck();
+    } else {
+      equalityCheck.innerHTML = "Passwords are equal ❌";
+      lowerCaseCheck.innerHTML = "Lower Case Letters ❌";
+      upperCaseCheck.innerHTML = "Upper Case Letters ❌";
+      containsNumbersCheck.innerHTML = "Contains Numbers ❌";
+      minimumLengthCheck.innerHTML = "At least 10 characters long ❌";
+    }
+  });
+
+  firstPassword.addEventListener("input", () => {
+    if (firstPassword.value === secondPassword.value) {
+      passwordCheck();
+    } else {
+      equalityCheck.innerHTML = "Passwords are equal ❌";
+      lowerCaseCheck.innerHTML = "Lower Case Letters ❌";
+      upperCaseCheck.innerHTML = "Upper Case Letters ❌";
+      containsNumbersCheck.innerHTML = "Contains Numbers ❌";
+      minimumLengthCheck.innerHTML = "At least 10 characters long ❌";
+    }
+  });
+
+  function passwordCheck() {
+    //First check: Passwords are euqal
+    if (firstPassword.value === secondPassword.value) {
+      equalityCheck.innerHTML = "Passwords are equal ✅";
+    } else {
+      equalityCheck.innerHTML = "Passwords are equal ❌";
+    }
+
     //Second check: Lower case letters
-    const lowerCaseCheck = document.querySelector("#lower-case-check");
     if (
       firstPassword.value.match(/[a-z]/gm) &&
       secondPassword.value.match(/[a-z]/gm)
@@ -39,8 +52,9 @@ function passwordCheck() {
     } else {
       lowerCaseCheck.innerHTML = "Lower Case Letters ❌";
     }
+
     //Third check: Upper case letters
-    const upperCaseCheck = document.querySelector("#upper-case-check");
+
     if (
       firstPassword.value.match(/[A-Z]/gm) &&
       secondPassword.value.match(/[A-Z]/gm)
@@ -49,10 +63,9 @@ function passwordCheck() {
     } else {
       upperCaseCheck.innerHTML = "Upper Case Letters ❌";
     }
+
     //Fourth check: Contains numbers
-    const containsNumbersCheck = document.querySelector(
-      "#contains-numbers-check"
-    );
+
     if (
       firstPassword.value.match(/[1-9]/gm) &&
       secondPassword.value.match(/[1-9]/gm)
@@ -63,13 +76,30 @@ function passwordCheck() {
     }
 
     //Fifth check: At least 10 characters long
-    const minimumLengthCheck = document.querySelector("#minimum-length-check");
+
     if (firstPassword.value.length > 10 && secondPassword.value.length > 10) {
       minimumLengthCheck.innerHTML = "At least 10 characters long ✅";
     } else {
       minimumLengthCheck.innerHTML = "At least 10 characters long ❌";
     }
-  } else {
-    equalityCheck.innerHTML = "Passwords are equal ❌";
   }
+
+  //Toggle button to show and hide passwords
+  const switchPWButton = document.querySelector("#switch-pw-visibility");
+  switchPWButton.addEventListener("click", () => {
+    const firstPassword = document.querySelector("#first-password");
+    const secondPassword = document.querySelector("#second-password");
+
+    if (firstPassword.type && secondPassword.type === "password") {
+      firstPassword.type = "text";
+      secondPassword.type = "text";
+      switchPWButton.innerText = "Hide Passwords";
+    } else if (firstPassword.type && secondPassword.type === "text") {
+      firstPassword.type = "password";
+      secondPassword.type = "password";
+      switchPWButton.innerText = "Show Passwords";
+    }
+  });
 }
+
+renderPasswordCheck();
